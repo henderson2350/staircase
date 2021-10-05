d2aa475f1b5823d9e482dc9b4796a1216ecfd6cf
Clare Henderson <henderson2350@gmail.com>
2021-10-05 15:01:29 -0400

    working but not right oriented

--- Staircase.java
+++ Staircase.java
@@ -1,3 +1,23 @@
-public class Staircase {
+import java.util.*;
 
+public class Staircase {
+    public static void StaircaseFunc(int n) {
+        //create an array filled n with spaces
+        List<String> myList  = new ArrayList<String>();
+
+        //iterate n times and each time replace the last item in the 
+        //array with a # and then print it out
+        for (int i=0; i<n; i++) {
+            myList.add(" ");
+        }
+
+        String myString = "";
+        for (int i=n -1; i>0; i--) {
+            myList.set(i, "#");
+
+            myString += (myList.get(i));
+
+            System.out.println(myString);
+        }
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
-        
+        Staircase myStaircase = new Staircase();
+        myStaircase.StaircaseFunc(8);
     }
 }
